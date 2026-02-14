a32bbf422ba68964f660cfea5a103070cf2a54b9
LV <viplong12hh@gmail.com>
2026-02-13 23:04:52 -0600

    Added: Crash test

--- app.js
+++ app.js
@@ -20,8 +20,13 @@ mongoose
 app.use(cors());
 app.use(express.json());
 
-app.use(requestLogger);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
 
+app.use(requestLogger);
 
 app.use("/", require("./routes/index"));
 
