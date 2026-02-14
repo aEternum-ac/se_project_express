0107a6cc783c23282212bc59542b9878686aa02d
LV <viplong12hh@gmail.com>
2026-02-13 23:23:06 -0600

    added helmet and limiter middlewares to app.js for security and rate limiting.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const {errors} = require('celebrate');
+const helmet = require("helmet");
+const limiter = require("./middlewares/limiter");
 const{ requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3001 } = process.env;
@@ -19,6 +21,9 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+
+app.use(helmet());
+app.use(limiter);
 
 app.get('/crash-test', () => {
   setTimeout(() => {
